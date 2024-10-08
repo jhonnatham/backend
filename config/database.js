2583df5b74783402891de602dd325e1fef799e96
jhonnatham <jhosamir86@gmail.com>
2024-10-07 20:19:08 -0500

    version example .env y ajuste conexion para bd local

--- config/database.js
+++ config/database.js
@@ -2,14 +2,16 @@ const { Client } = require('pg')
 
 require('dotenv').config()
 
+const configssl = process.env.BD_SSL = 'false' ? false: true
 const connectionData = {
     user: process.env.BD_USER,
     host: process.env.BD_HOST,
     database: process.env.BD_DATABASE,
     password: process.env.BD_PASSWORD,
     port: process.env.BD_PORT,
-    ssl: process.env.BD_SSL
+    ssl: configssl
 }
+
 const client = new Client(connectionData)
 
 client.connect()
